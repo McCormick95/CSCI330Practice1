82ea7b55e37f739795c6a637bc279d0605d9605d
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-01-18 22:23:11 +0000

    Saving at the completion of Practing C# 1 video.

---
+++ sample1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Show one = new Show() {
+                Name= "Parks and Recreation",
+                NumEpisodes= 125,
+                Year=2009,
+                Actors= new List<string> {
+                    "Amy Poehler","Aziz Ansari","Nick Offerman"
+                }
+            };
+
+            Console.WriteLine(one);
+        }
+    }
+}
