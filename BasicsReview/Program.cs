0914cc772bd5fab41aa6e85b5479f6534d25fed1
McCormick95 <106946540+McCormick95@users.noreply.github.com>
2023-01-19 22:03:29 +0000

    Saved after completion of List Collection Tutorial

--- BasicsReview/Program.cs
+++ BasicsReview/Program.cs
@@ -78,4 +78,72 @@ void ExploreLoops(){
     }
 }
 
+//WorkWithStrings();
+
+void WorkWithStrings(){
+
+    Console.WriteLine("-----------------------------------------------");
+
+    var names = new List<string> {"Ryan", "Ana", "Felipe"};
+    foreach(var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine("-----------------------------------------------");
+
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Ana");
+
+    foreach(var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine("-----------------------------------------------");
+
+    Console.WriteLine($"My name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+    Console.WriteLine($"This list has {names.Count} people in it.");
+
+    Console.WriteLine("-----------------------------------------------");
+
+    var index = names.IndexOf("Ryan");
+    if( index ==-1){
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else{
+        Console.WriteLine($"The name {names[index]} is at index: {index}");
+    }
+
+    index = names.IndexOf("Not Found");
+    if(index == -1){
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else{
+        Console.WriteLine($"The name {names[index]} is at index: {index}");
+    }
+
+    Console.WriteLine("-----------------------------------------------");
+
+    names.Sort();
+    foreach(var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+}
+
+Console.WriteLine("-----------------------------------------------");
+
+var fibonacciNumbers = new List<int> {1,1};
+
+while(fibonacciNumbers.Count < 20){
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+
+foreach(var item in fibonacciNumbers){
+        Console.WriteLine(item);
+}
+
 
